9babc7aceb53411a036beff34d02dbb8c4405619
Eldon-498 <eldondaniel2@gmail.com>
2023-07-01 18:23:43 +0300

    cleanup

--- test-app/db.js
+++
@@ -1,6 +0,0 @@
-const { Datastore } = require('notarealdb');
-const store = new Datastore('./data');
-module.exports = {
-    students: store.collection('students'),
-    colleges: store.collection('colleges')
-};
