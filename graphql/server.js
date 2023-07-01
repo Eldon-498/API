0bc4a23612eba26a3cd59f772359d1ad40a86b89
Eldon-498 <eldondaniel2@gmail.com>
2023-07-01 20:33:49 +0300

    Welcome message

--- graphql/server.js
+++ graphql/server.js
@@ -7,15 +7,20 @@ const app = express();
 const schema = buildSchema(`
 type Query {
     hello: String
-}
+    welcomeMessage(name: String): String
 
+}
 `)
 
 const root = {
     hello: () => {
         return 'hello world';
     },
-}
+    welcomeMessage: (args) => {
+        console.log(args);
+        return `Hey ${args.name}, how\'s life`;
+    },
+};
 
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
