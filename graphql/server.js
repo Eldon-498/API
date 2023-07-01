7c4e6c76c0add8b41a72ef159f2f409a688af1f4
Eldon-498 <eldondaniel2@gmail.com>
2023-07-01 19:03:04 +0300

    Schema definition

---
+++ graphql/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const {buildSchema} = require('graphql');
+const {graphqlHTTP} = require('express-graphql');
+
+const app = express();
+
+const schema = buildSchema(`
+type Query {
+    hello: String
+}
+
+`)
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema,
+    rootValue: root
+
+}))
+
+app.listen(4000, () => console.log("Server on port 4000"));
