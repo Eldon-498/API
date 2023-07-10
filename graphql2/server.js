5837b76cefa01cd96c408e9b48d52a2b1bea1ef8
Eldon-498 <eldondaniel2@gmail.com>
2023-07-10 23:06:46 +0300

    Testing from query studio

--- graphql2/server.js
+++ graphql2/server.js
@@ -8,10 +8,31 @@ type Query{
 
 
 `
-const resolver = {
+const resolvers = {
     Query: {
     hello: () => {
        return 'hello world';
     }
 }
 }
+
+async function startServer (){
+    const app = express();
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers
+    });
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({app: app});
+
+    app.use((req, res) => {
+        res.send("Hello from express apollo server");
+    })
+
+    app.listen(4000, () => console.log("Server running on port 4000"));
+
+
+}
+startServer();
